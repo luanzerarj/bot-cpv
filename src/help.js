7a13936628e0ba03bb23e99285e3bd0ad74cd4b4
luanzerarj <78701613+luanzerarj@users.noreply.github.com>
2021-02-07 20:02:02 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,32 @@
+const help = (prefix) => {
+	return `▶ *Lista de Comandos Ze Jacare CPV* ◀\n\n
+●●● *Comandos para Membros Comuns (KKKKKKKKKKK)* ●●●\n
+Comando: *${prefix}sticker*
+*Para fazer um sticker de imagem/gif/video*\n
+Comando: *${prefix}sticker nobg*
+*Para fazer um sticker sem o fundo branco*\n
+Comando: *${prefix}toimg*
+*Para converter um sticker p/ imagem*\n
+Comando: *${prefix}tsticker*
+*Para converter um texto para sticker*\n
+Comando: *${prefix}gtts*
+*Para converter um texto em audio*
+Exemplo: *${prefix}gtts [cc] [texto]*\n
+Comando: *${prefix}ocr*
+*Para o converter o texto da imagem*\n\n
+●●● *Comandos para Administracao Foda da CPV* ●●●\n
+Comando : *${prefix}add*
+*Para adicionar um membro.*\n
+Exemplo: *${prefix}add 5521900000000*\n
+Comando: *${prefix}kick*
+*Para remover um membro*\n
+Comando: *${prefix}promote*
+*Para tornar um membro como Administrador do grupo*\n
+Comando: *${prefix}demote*
+*Para remover um membro como Administrador do grupo*\n
+command : *${prefix}linkgroup*
+*Para visualizar o link de acesso do grupo*\n\n
+*_Reprogramadores: Betin e Luanzera_*\n`
+}
+
+exports.help = help
