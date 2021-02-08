14c7170c46d2aa021374e9fca48ecdc52d1ff57e
luanzerarj <78701613+luanzerarj@users.noreply.github.com>
2021-02-08 15:58:19 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,51 @@
+const help = (prefix) => {
+	return `◆ *LISTA DE COMANDOS DO ZE JACARE CPV*🤖 ◆\n
+⠀⠀_*${prefix}comandos*_\n
+⠀⠀_Atualizado em: 08/02/2021 as 15h30_\n
+
+➤ *${prefix}sticker*\n
+⠀⠀_Para fazer um sticker de Imagem ou GIF 👀._
+➤ *${prefix}voz pt texto*\n
+⠀⠀_Para o Ze Jacare falar alguma coisa 🗣️._
+➤ *${prefix}link*\n
+⠀⠀_Para gerar o link do grupo 🔗._
+➤ *${prefix}imagem*\n
+⠀⠀_Para transformar uma figurinha em imagem 📸._
+➤ *${prefix}tiktokstalk*
+⠀⠀_Para saber as informacoes de um usuario do TikTok 🤳._
+➤ *${prefix}mamei*\n
+⠀⠀_Para mamar gostoso kkkkkkkkkkk 🍆💦._
+➤ *${prefix}messidatrufa*\n
+⠀⠀_Musica do messi 🍫._
+➤ *${prefix}teste*\n
+⠀⠀_Para testar o funcionamento do Ze Jacare ✅._
+➤ *${prefix}puta*\n
+⠀⠀_Para ouvir um trecho da musica de YungGott 🎶🎹._
+➤ *${prefix}clonacartao*\n
+⠀⠀_Para clonar um cartao 💳😈._
+➤ *${prefix}matlastore*\n
+⠀⠀_Para acessar a melhor loja de roupas esportivas do Brasil 👕⚽🏀._
+➤ *${prefix}kkk*\n
+⠀⠀_Para fazer o BOT rir com KKKKKKK 🤣🤣._
+➤ *${prefix}perdi*\n
+⠀⠀_Para fazer o BOT perder 🅿️._
+➤ *${prefix}explodi*\n
+⠀⠀_Para fazer o BOT explodir 💥._\n
+➤ *${prefix}textaodopedroka*\n
+⠀⠀_Assim como betin...🚶_
+➤ *${prefix}textaodanilo*\n
+⠀⠀_Gabiru eu to ligado que vc é frustrado...😞_
+➤ *${prefix}textaogabiru*\n
+⠀⠀_Cara, tu acha que eu sou frustrado?...😖_
+➤ *${prefix}textaolevi*\n
+⠀⠀_Esse rapaz n tem o mínimo de QI...🤓🧠_\n
+
+◆ *ADMINISTRAÇÃO* ◆
+➤ *${prefix}listadms\n
+➤ *${prefix}add\n
+➤ *${prefix}sadm\n
+➤ *${prefix}ban\n
+➤ *${prefix}welcome 1/2\n`
+}
+
+exports.help = help
