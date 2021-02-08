b4a231fd4786c4f3fdd62a66a46d1776ceac88aa
luanzerarj <78701613+luanzerarj@users.noreply.github.com>
2021-02-07 22:46:58 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,46 @@
+const help = (prefix) => {
+	return `▶ *Lista de /comandos Ze Jacare CPV* ◀\n
+● *Comandos para Membros Comuns kkkkkkkkk* ●\n
+╔• Comando: *${prefix}sticker*
+╠- *_Para fazer um sticker de imagem/gif/video_*
+╠• Comando: *${prefix}sticker nobg*
+╠- *_Para fazer um sticker sem o fundo branco_*
+╠• Comando: *${prefix}imagem*
+╠- *_Para converter um sticker p/ imagem_*
+╠• Comando: *${prefix}tsticker*
+╠- *_Para converter um texto para sticker_*
+╠• Comando: *${prefix}gtts*
+╠- *_Para converter um texto em audio_*
+╠- *_Exemplo:_* *${prefix}gtts pt texto*
+╠• Comando: *${prefix}mp3*
+╠- *_Para baixar um video do youtube em MP3_*
+╠• Comando: *${prefix}ocr*
+╠- *_Para o converter o texto da imagem_*
+╠• Comando: *${prefix}mamei*
+╚- *_Pra mamar gostoso kkkkkkk_*\n\n\n
+//╠• Comando: *${prefix}utilizar*
+//╠- *_Para ser utilizado_*
+//╠• Comando: *${prefix}utilizar*
+//╠- *_Para ser utilizado_*
+//╠• Comando: *${prefix}utilizar*
+//╠- *_Para ser utilizado_*
+//╠• Comando: *${prefix}utilizar*
+//╠- *_Para ser utilizado_*
+● *Comandos para Administracao Foda da CPV* ●\n
+╔• Comando: *${prefix}add*
+╠- *_Para adicionar um membro._*
+╠- *_Exemplo:_* *${prefix}add 5521900000000*
+╠• Comando: *${prefix}listadms*
+╠- *_Para mostrar a lista de ADMs da CPV._*
+╠• Comando: *${prefix}ban*
+╠- *_Para remover um membro_*
+╠• Comando: *${prefix}adm*
+╠- *_Para tornar um membro como Administrador do grupo_*
+╠• Comando: *${prefix}sadm*
+╠- *_Para remover um membro como Administrador do grupo_*
+╠• Comando: *${prefix}link*
+╚- *_Para visualizar o link de acesso do grupo_*\n\n\n
+*_Reprogramadores: Betin e Luanzera_*\n`
+}
+
+exports.help = help
