27663ad9fc5a24c14e3001de487544584e8aaed5
luanzerarj <78701613+luanzerarj@users.noreply.github.com>
2021-03-06 17:56:31 -0300

    Add files via upload

--- plugins/attp.js
+++ plugins/attp.js
@@ -3,20 +3,20 @@ const FormData = require('form-data')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler  = async (m, { conn, text }) => {
-  if (text) conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, false, { asSticker: true })
+  if (text) conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, true, { asSticker: true })
   else throw 'Uhm...Teksnya?'
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
 handler.command = /^attp$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.owner = true
+handler.mods = true
+handler.premium = true
+handler.group = true
+handler.private = true
 
-handler.admin = false
-handler.botAdmin = false
+handler.admin = true
+handler.botAdmin = true
 
 handler.fail = null
 
